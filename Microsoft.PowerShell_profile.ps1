99bd8a8d27cdd2486ada5996baafc5e4dff883f8
Andreas Kössler <github@akoessler.com>
2021-02-14 22:40:47 +0100

    Add support for custom functions defined in files

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,9 @@
+Write-Host "Loading profile ..."
 
 Import-Module posh-git
 Import-Module oh-my-posh
 
-$ProfileScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+$ProfileScriptDir = $PSScriptRoot;
 
 Set-PoshPrompt -Theme $ProfileScriptDir\oh-my-posh-theme.json
 
@@ -10,3 +11,23 @@ if ($host.Name -eq 'ConsoleHost')
 {
     Import-Module PSReadLine
 }
+
+Write-Host "Load custom scripts ..."
+
+$customFunctionsScriptPath = "$ProfileScriptDir\custom-functions\"
+$customFunctionsScriptPathMachine = "$ProfileScriptDir\custom-functions\$env:COMPUTERNAME"
+
+if (Test-Path $customFunctionsScriptPathMachine) {
+    $customFunctionsScripts = Get-ChildItem -File -Path $customFunctionsScriptPath,$customFunctionsScriptPathMachine
+} else {
+    $customFunctionsScripts = Get-ChildItem -File -Path $customFunctionsScriptPath
+}
+
+foreach($script in $customFunctionsScripts) {
+    $scriptName = ($script.FullName -replace [regex]::Escape($customFunctionsScriptPath), "")
+    Write-Host "  Import $scriptName"
+    Import-Module $script.FullName
+}
+
+Write-Host "Profile load complete." -ForegroundColor Green
+Write-Host ""

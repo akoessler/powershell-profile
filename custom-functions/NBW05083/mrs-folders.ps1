2bfa16d994fc4a60ef987fee7e3e5b11de0b3a26
Andreas Kössler <github@akoessler.com>
2021-02-14 23:58:53 +0100

    Add custom functions for mrs shortcuts

---
+++ custom-functions/NBW05083/mrs-folders.ps1
@@ -0,0 +1,36 @@
+
+function mrs() {
+    Set-Location -Path "D:\dev\mrs"
+}
+
+function mrsapi() {
+    Set-Location -Path "D:\dev\mrs\api"
+}
+
+function mrsapp() {
+    Set-Location -Path "D:\dev\mrs\app"
+}
+
+function mrslib() {
+    Set-Location -Path "D:\dev\mrs\lib"
+}
+
+function mrsportable() {
+    Set-Location -Path "D:\dev\mrs\api\portable-api"
+}
+
+function mrsedes() {
+    Set-Location -Path "D:\dev\mrs\api\edes-api"
+}
+
+function mrsgateway() {
+    Set-Location -Path "D:\dev\mrs\app\ccs.gateway.edes-app"
+}
+
+function mrsauth() {
+    Set-Location -Path "D:\dev\mrs\app\auth-app"
+}
+
+function mrsmission() {
+    Set-Location -Path "D:\dev\mrs\app\mission-app"
+}

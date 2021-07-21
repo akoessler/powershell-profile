b8b33b1072b7e97303d85d417520c7ae83524366
Andreas Kössler <github@akoessler.com>
2021-07-21 19:12:38 +0200

    Improve profile loading, add stopwatch

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,44 @@
-Write-Host "Loading profile ..."
+Write-Host "Loading profile ..." -ForegroundColor Cyan
+$StopwatchProfile = [System.Diagnostics.Stopwatch]::StartNew()
 
-Import-Module posh-git
-Import-Module oh-my-posh
+
+Write-Host "  Init basic stuff ..." -ForegroundColor DarkYellow
+$StopwatchInit = [System.Diagnostics.Stopwatch]::StartNew()
+
+
+Write-Host "    Import modules" -NoNewLine -ForegroundColor DarkGray
+$StopwatchInitDetail = [System.Diagnostics.Stopwatch]::StartNew()
+
+if ($Global:CustomProfileModulesAreLoaded = $false)
+{
+    Import-Module posh-git -Global
+    Import-Module oh-my-posh -Global
+    $Global:CustomProfileModulesAreLoaded = $true
+}
+
+Write-Host (" - done (" + $StopwatchInitDetail.ElapsedMilliseconds + "ms)") -ForegroundColor DarkGray
+
+
+Write-Host "    Set settings" -NoNewLine -ForegroundColor DarkGray
+$StopwatchInitDetail = [System.Diagnostics.Stopwatch]::StartNew()
 
 Set-StrictMode -Version Latest
 
 $ProfileScriptDir = $PSScriptRoot;
 
+Write-Host (" - done (" + $StopwatchInitDetail.ElapsedMilliseconds + "ms)") -ForegroundColor DarkGray
+
+
+Write-Host "    Load oh-my-posh" -NoNewLine -ForegroundColor DarkGray
+$StopwatchInitDetail = [System.Diagnostics.Stopwatch]::StartNew()
+
 Set-PoshPrompt -Theme $ProfileScriptDir\oh-my-posh-theme.json
+
+Write-Host (" - done (" + $StopwatchInitDetail.ElapsedMilliseconds + "ms)") -ForegroundColor DarkGray
+
+
+Write-Host "    Load PSReadLine" -NoNewLine -ForegroundColor DarkGray
+$StopwatchInitDetail = [System.Diagnostics.Stopwatch]::StartNew()
 
 if ($host.Name -eq 'ConsoleHost')
 {
@@ -16,7 +47,14 @@ if ($host.Name -eq 'ConsoleHost')
     Set-PSReadLineKeyHandler -Key Ctrl+DownArrow -Function HistorySearchForward
 }
 
-Write-Host "Load custom scripts ..."
+Write-Host (" - done (" + $StopwatchInitDetail.ElapsedMilliseconds + "ms)") -ForegroundColor DarkGray
+
+
+Write-Host ("  Init finished (" + $StopwatchInit.ElapsedMilliseconds + "ms)") -ForegroundColor DarkGreen
+
+
+Write-Host "  Load custom scripts ..." -ForegroundColor DarkYellow
+$StopwatchScripts = [System.Diagnostics.Stopwatch]::StartNew()
 
 $customFunctionsScriptPath = "$ProfileScriptDir\custom-functions\"
 $customFunctionsScriptPathMachine = "$ProfileScriptDir\custom-functions\$env:COMPUTERNAME"
@@ -29,9 +67,14 @@ if (Test-Path $customFunctionsScriptPathMachine) {
 
 foreach($script in $customFunctionsScripts) {
     $scriptName = ($script.FullName -replace [regex]::Escape($customFunctionsScriptPath), "")
-    Write-Host "  Import $scriptName"
+    Write-Host "    Import $scriptName" -NoNewLine -ForegroundColor DarkGray
+    $StopwatchImport = [System.Diagnostics.Stopwatch]::StartNew()
     Import-Module $script.FullName
+    Write-Host (" - done (" + $StopwatchImport.ElapsedMilliseconds + "ms)") -ForegroundColor DarkGray
 }
 
-Write-Host "Profile load complete." -ForegroundColor Green
+Write-Host ("  Custom scripts finished (" + $StopwatchScripts.ElapsedMilliseconds + "ms)") -ForegroundColor DarkGreen
+
+
+Write-Host ("Profile load complete (" + $StopwatchProfile.ElapsedMilliseconds + "ms)") -ForegroundColor Cyan
 Write-Host ""

af37cc421b58f3fbc0c72227b3ef29fef5e7a9c7
Andreas Kössler <github@akoessler.com>
2021-12-03 18:44:56 +0100

    Rework install script, use current user, apply -y for choco

--- install-everything.ps1
+++ install-everything.ps1
@@ -5,20 +5,21 @@ Set-ExecutionPolicy RemoteSigned -Scope MachinePolicy
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-Install-Module -Force -Scope AllUsers -AllowPrerelease -Name PSColor
-Install-Module -Force -Scope AllUsers -AllowPrerelease -Name PSReadLine
-Install-Module -Force -Scope AllUsers -AllowPrerelease -Name PSScriptTools
-Install-Module -Force -Scope AllUsers -AllowPrerelease -Name Terminal-Icons
-Install-Module -Force -Scope AllUsers -AllowPrerelease -Name ZLocation
-Install-Module -Force -Scope AllUsers -AllowPrerelease -Name PoShFuck
-Install-Module -Force -Scope AllUsers -AllowPrerelease -Name Posh-SSH
-Install-Module -Force -Scope AllUsers -AllowPrerelease -Name posh-git
-Install-Module -Force -Scope AllUsers -AllowPrerelease -Name oh-my-posh
+Install-Module -Force -Scope CurrentUser -AllowPrerelease -Name PSColor
+Install-Module -Force -Scope CurrentUser -AllowPrerelease -Name PSReadLine
+Install-Module -Force -Scope CurrentUser -AllowPrerelease -Name PSScriptTools
+Install-Module -Force -Scope CurrentUser -AllowPrerelease -Name Terminal-Icons
+Install-Module -Force -Scope CurrentUser -AllowPrerelease -Name ZLocation
+Install-Module -Force -Scope CurrentUser -AllowPrerelease -Name Posh-SSH
+Install-Module -Force -Scope CurrentUser -AllowPrerelease -Name posh-git
+Install-Module -Force -Scope CurrentUser -AllowPrerelease -Name oh-my-posh
 
 Invoke-Expression ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mattparkes/PoShFuck/master/Install-TheFucker.ps1'))
 
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install choco-upgrade-all-at-startup
-choco install sudo
-choco install bat
+choco install choco-upgrade-all-at-startup -y
+choco install sudo -y
+choco install bat -y
+
+choco upgrade all -y

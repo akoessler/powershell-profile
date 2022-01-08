924eb9df6c49cc358628c914c1a0bbd9514ffd37
Andreas Kössler <github@akoessler.com>
2022-01-08 21:34:10 +0100

    Fix target scope of module install in Update-AllModules

--- custom-functions/Update-AllModules.psm1
+++ custom-functions/Update-AllModules.psm1
@@ -3,14 +3,17 @@ function Update-AllModules() {
 
     foreach ($installedModule in $installedModules) {
         $moduleName = $installedModule.Name
+        $modulePath = $installedModule.Path
         $currentVersion = $installedModule.Version
-        $isPrerelease = $installedModule.Version -Match "-"
+
+        $isPrerelease = $currentVersion -Match "-"
+        $isUserScope = $modulePath -Match "C:\\Users\\"
 
         if ($isPrerelease) {
-            Write-Host -ForegroundColor White "Check module $moduleName - installed: $currentVersion (prerelease) ..."
+            Write-Host -ForegroundColor White "Check module $moduleName - installed: $currentVersion (prerelease) ... $modulePath"
             $moduleInfos = Find-Module -Name $installedModule.Name -AllowPrerelease
         } else {
-            Write-Host -ForegroundColor White "Check module $moduleName - installed: $currentVersion ..."
+            Write-Host -ForegroundColor White "Check module $moduleName - installed: $currentVersion ... $modulePath"
             $moduleInfos = Find-Module -Name $installedModule.Name
         }
 
@@ -18,16 +21,13 @@ function Update-AllModules() {
         $updateDate = $moduleInfos.PublishedDate
 
         if ($updateVersion -eq $currentVersion) {
-            Write-Host -ForegroundColor Green " $moduleName already installed in the latest version $currentVersion ($updateDate)"
+            Write-Host -ForegroundColor Green "  $moduleName already installed in the latest version $currentVersion ($updateDate)"
         }
         else {
-            Write-Host -ForegroundColor Cyan " $moduleName - Update from $currentVersion to $updateVersion ($updateDate)" 
+            $targetScope = $isUserScope ? "CurrentUser" : "AllUsers"
+            Write-Host -ForegroundColor Cyan "  $moduleName - Update from $currentVersion to $updateVersion ($updateDate) - prerelease:$isPrerelease, scope:$targetScope"
             try {
-                if ($isPrerelease) {
-                    Update-Module -Name $moduleName -Force -Scope AllUsers -AllowPrerelease
-                } else {
-                    Update-Module -Name $moduleName -Force -Scope AllUsers
-                }
+                Update-Module -Name $moduleName -Force -Scope $targetScope -AllowPrerelease:$isPrerelease -Verbose
             }
             catch {
                 Write-Host -ForegroundColor Red $_.Exception.Message

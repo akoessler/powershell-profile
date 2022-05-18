574737495626ff46b1f1079fde16c5a410d176ff
Andreas Kössler <github@akoessler.com>
2022-05-18 11:42:11 +0200

    Add checkout default branch option to pullall

--- custom-functions/Update-AllRepos.psm1
+++ custom-functions/Update-AllRepos.psm1
@@ -1,5 +1,5 @@
 
-function __LoopSubfolders_updaterepos([String] $Directory, [Int32] $MaxNestingLevel, [Int32] $NestingLevel)
+function __LoopSubfolders_updaterepos([String] $Directory, [Boolean] $CheckoutDefaultBranch = $false, [Int32] $MaxNestingLevel, [Int32] $NestingLevel)
 {
     Write-Verbose -Message ""
     Write-Verbose -Message ("=====  " + $Directory)
@@ -14,15 +14,37 @@ function __LoopSubfolders_updaterepos([String] $Directory, [Int32] $MaxNestingLe
 
             Set-Location $_.FullName
             Write-Host ""
-            Write-Host ("> " + $_.FullName)
+            Write-Host ("> " + $_.FullName) -ForegroundColor Cyan
 
             Write-Host ""
-            Invoke-Expression "git branch --show-current"
+            Invoke-Expression "git branch --show-current" | Tee-Object -Variable BranchName | out-null
 
-            Write-Host ""
+            Write-Host ("current branch: " + $BranchName) -ForegroundColor Yellow
+
+            if ($CheckoutDefaultBranch)
+            {
+                Invoke-Expression "git config init.defaultBranch" | Tee-Object -Variable DefaultBranchName | out-null
+                if ([string]::IsNullOrEmpty($DefaultBranchName))
+                {
+                    $DefaultBranchName = "master"
+                }
+
+                if ($BranchName -eq $DefaultBranchName)
+                {
+                    Write-Host "already default branch" -ForegroundColor Green
+                }
+                else
+                {
+                    $GitCommand = ("git checkout " + $DefaultBranchName)
+                    Write-Host $GitCommand -ForegroundColor Magenta
+                    Invoke-Expression $GitCommand
+                }
+
+                Write-Host ""
+            }
 
             $GitCommand = "git pull"
-            Write-Verbose -Message $GitCommand
+            Write-Host -Message $GitCommand -ForegroundColor Magenta
             Invoke-Expression $GitCommand
 
             Write-Host ""
@@ -42,7 +64,7 @@ function __LoopSubfolders_updaterepos([String] $Directory, [Int32] $MaxNestingLe
     Write-Verbose ""
 }
 
-function Update-AllRepos([String] $Path, [Int32] $MaxNestingLevel = 1)
+function Update-AllRepos([String] $Path, [Boolean] $CheckoutDefaultBranch = $false, [Int32] $MaxNestingLevel = 1)
 {
     $CurrentLocation = (Get-Location).path
 
@@ -54,7 +76,7 @@ function Update-AllRepos([String] $Path, [Int32] $MaxNestingLevel = 1)
 
     try
     {
-        __LoopSubfolders_updaterepos $Path $MaxNestingLevel 0
+        __LoopSubfolders_updaterepos $Path $CheckoutDefaultBranch $MaxNestingLevel 0
     }
     finally
     {
@@ -64,6 +86,13 @@ function Update-AllRepos([String] $Path, [Int32] $MaxNestingLevel = 1)
     Write-Verbose -Message "Done."
 }
 
+function Update-AllReposAndCheckoutDefaultBranch([String] $Path)
+{
+    Update-AllRepos $Path $true
+}
+
 Set-Alias -Name pullall -Value Update-AllRepos
+Set-Alias -Name pullallmaster -Value Update-AllReposAndCheckoutDefaultBranch
 
 Export-ModuleMember -Function Update-AllRepos -Alias pullall
+Export-ModuleMember -Function Update-AllReposAndCheckoutDefaultBranch -Alias pullallmaster

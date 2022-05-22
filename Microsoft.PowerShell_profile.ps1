a539f653be8256930ffbbf883b460c42b7d1087d
Andreas Kössler <github@akoessler.com>
2022-05-22 02:05:51 +0200

    Remove timing function from profile and installer
    The function based approach with ScriptBlock raises issues with scopes.
    Recently this even breaks oh-my-posh.
    So move back to normal code, but keep minimal timing approach
    with Begin/End functions.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,109 +1,82 @@
-$ExecuteTimedIndentation = 0;
-function ExecuteTimed([String] $Title, [ConsoleColor] $Color, [bool] $NewLine, [ScriptBlock] $Script) {
-    $Indent = " " * $ExecuteTimedIndentation
-    $script:ExecuteTimedIndentation = $ExecuteTimedIndentation + 2;
-
-    if ($NewLine) {
-        Write-Host ($Indent + $Title + " ...") -ForegroundColor $Color
-    } else {
-        Write-Host ($Indent + $Title) -ForegroundColor $Color -NoNewline
-    }
-
-    $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-
-    try {
-        $Script.Invoke();
-    }
-    catch {
-        Write-Host -ForegroundColor Red $_.Exception.Message
-    }
-
-    $Stopwatch.Stop();
-
-    if ($NewLine) {
-        Write-Host ($Indent + "finished (" + $Stopwatch.ElapsedMilliseconds + "ms)") -ForegroundColor $Color
-    } else {
-        Write-Host (" - done (" + $Stopwatch.ElapsedMilliseconds + "ms)") -ForegroundColor $Color
-    }
-
-    $script:ExecuteTimedIndentation = $ExecuteTimedIndentation - 2;
+function private:Header([String] $Title) {
+    Write-Host $Title -ForegroundColor Magenta
 }
 
-ExecuteTimed "Loading profile" Cyan $True {
-
-    ExecuteTimed "Imports" DarkMagenta $True {
-
-        $ModulesToImport = @(
-            "Terminal-Icons"
-            "PSColor"
-            "Posh-SSH"
-            "ZLocation"
-            "Chocolatey"
-            "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-        )
-
-        if ($IsWindows) {
-            $WindowsOnlyModules = @(
-                "posh-git"
-                "PoShFuck"
-            )
-            $ModulesToImport = $WindowsOnlyModules + $ModulesToImport
-        }
-
-        foreach($ModuleToImport in $ModulesToImport) {
-            ExecuteTimed ("Import " + $ModuleToImport) DarkGray $false {
-                Import-Module $ModuleToImport
-            }
-        }
-
-    }
-
-    ExecuteTimed "Init" DarkMagenta $True {
-
-        ExecuteTimed "Set Variables" DarkGray $False {
-            Set-StrictMode -Version Latest
-            $script:ProfileScriptDir = $PSScriptRoot;
-            $env:POSH_GIT_ENABLED = $true
-        }
-
-        ExecuteTimed "Load oh-my-posh" DarkGray $false {
-            oh-my-posh init pwsh --config $ProfileScriptDir\oh-my-posh-theme.json | Invoke-Expression
-        }
-
-        ExecuteTimed "Load PSReadLine" DarkGray $false {
-            if ($host.Name -eq 'ConsoleHost') {
-                Import-Module PSReadLine
-                Set-PSReadLineOption -EditMode Windows
-                Set-PSReadLineOption -PredictionSource History
-                Set-PSReadLineOption -PredictionViewStyle ListView
-                Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-                Set-PSReadLineKeyHandler -Key Ctrl+UpArrow -Function HistorySearchBackward
-                Set-PSReadLineKeyHandler -Key Ctrl+DownArrow -Function HistorySearchForward
-            } else {
-                Write-Host " - skipped" - -ForegroundColor DarkGray -NoNewline
-            }
-        }
-
-    }
-
-    ExecuteTimed "Load custom scripts" DarkMagenta $True {
-
-        $Hostname = [system.environment]::MachineName
-        $CustomFunctionsScriptPath = "$ProfileScriptDir\custom-functions\"
-        $CustomFunctionsScriptPathMachine = "$ProfileScriptDir\custom-functions\$Hostname"
-
-        if (Test-Path $customFunctionsScriptPathMachine) {
-            $CustomFunctionsScripts = Get-ChildItem -File -Filter *.psm1 -Path $CustomFunctionsScriptPath,$CustomFunctionsScriptPathMachine
-        } else {
-            $CustomFunctionsScripts = Get-ChildItem -File -Filter *.psm1 -Path $CustomFunctionsScriptPath
-        }
-
-        foreach($CustomFunctionScript in $CustomFunctionsScripts) {
-            $ScriptName = ($CustomFunctionScript.FullName -replace [regex]::Escape($CustomFunctionsScriptPath), "")
-            ExecuteTimed ("Import " + $ScriptName) DarkGray $false {
-                Import-Module $CustomFunctionScript.FullName
-            }
-        }
-
-    }
+function private:Begin([String] $Title) {
+    Write-Host $Title -ForegroundColor Gray -NoNewline
+    $script:Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 }
+
+function private:End() {
+    $script:Stopwatch.Stop()
+    Write-Host " - done: $($script:Stopwatch.ElapsedMilliseconds)ms" -ForegroundColor DarkGreen
+}
+
+$ProfileScriptDir = $PSScriptRoot;
+$env:POSH_GIT_ENABLED = $true
+
+Header "Import modules ..."
+
+$ModulesToImport = @(
+    "Terminal-Icons"
+    "PSColor"
+    "Posh-SSH"
+    "ZLocation"
+    "Chocolatey"
+    "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+)
+
+if ($IsWindows) {
+    $WindowsOnlyModules = @(
+        "posh-git"
+        "PoShFuck"
+    )
+    $ModulesToImport = $WindowsOnlyModules + $ModulesToImport
+}
+
+foreach($ModuleToImport in $ModulesToImport) {
+    Begin "Import $ModuleToImport"
+    Import-Module $ModuleToImport
+    End
+}
+
+Header "Load custom scripts ..."
+
+$Hostname = [system.environment]::MachineName
+$CustomFunctionsScriptPath = "$ProfileScriptDir\custom-functions\"
+$CustomFunctionsScriptPathMachine = "$ProfileScriptDir\custom-functions\$Hostname"
+
+if (Test-Path $customFunctionsScriptPathMachine) {
+    $CustomFunctionsScripts = Get-ChildItem -File -Filter *.psm1 -Path $CustomFunctionsScriptPath,$CustomFunctionsScriptPathMachine
+} else {
+    $CustomFunctionsScripts = Get-ChildItem -File -Filter *.psm1 -Path $CustomFunctionsScriptPath
+}
+
+foreach($CustomFunctionScript in $CustomFunctionsScripts) {
+    Begin "Import $($CustomFunctionScript.Directory.BaseName)/$($CustomFunctionScript.BaseName)"
+    Import-Module $CustomFunctionScript.FullName
+    End
+}
+
+Header "Load special tools ..."
+
+Begin "Load oh-my-posh"
+oh-my-posh init pwsh --config $ProfileScriptDir\oh-my-posh-theme.json | Invoke-Expression
+End
+
+Begin "Load PSReadLine"
+if ($host.Name -eq 'ConsoleHost') {
+    Import-Module PSReadLine
+    Set-PSReadLineOption -EditMode Windows
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -PredictionViewStyle ListView
+    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+    Set-PSReadLineKeyHandler -Key Ctrl+UpArrow -Function HistorySearchBackward
+    Set-PSReadLineKeyHandler -Key Ctrl+DownArrow -Function HistorySearchForward
+} else {
+    Write-Host " - skipped" - -ForegroundColor DarkGray -NoNewline
+}
+End
+
+Header "Finished."
+Write-Host ""
--- install-everything.ps1
+++ install-everything.ps1
@@ -1,114 +1,113 @@
 #Requires -RunAsAdministrator
 #must be admin for chocolatey
 
-function ExecuteTimed([String] $Title, [ConsoleColor] $Color, [ScriptBlock] $Script) {
-    Write-Host ($Title + " ...") -ForegroundColor $Color
-    $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-
-    try {
-        $Script.Invoke();
-    }
-    catch {
-        Write-Host -ForegroundColor Red $_.Exception.Message
-    }
-
-    $Stopwatch.Stop();
-    Write-Host ("finished (" + $Stopwatch.ElapsedMilliseconds + "ms)") -ForegroundColor $Color
+function private:Header([String] $Title) {
+    Write-Host ""
+    Write-Host "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" -ForegroundColor Blue
+    Write-Host ""
+    Write-Host $Title -ForegroundColor Magenta
 }
 
-ExecuteTimed "Set ExecutionPolicy" Cyan {
-    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-    Set-ExecutionPolicy RemoteSigned -Scope LocalMachine
-    Set-ExecutionPolicy RemoteSigned -Scope MachinePolicy
+function private:Begin([String] $Title) {
+    Write-Host ""
+    Write-Host $Title -ForegroundColor Cyan
+    Write-Host ""
+    $script:Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 }
 
-ExecuteTimed "Set global options" Cyan {
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+function private:End() {
+    $script:Stopwatch.Stop()
+    Write-Host ""
+    Write-Host ("  ... finished: " + $script:Stopwatch.ElapsedMilliseconds + "ms") -ForegroundColor Green
+    Write-Host ""
 }
 
-ExecuteTimed "Install PowerShell modules from gallery" Cyan {
-    $ModulesToInstall = @(
-        "PSColor"
-        "PSReadLine"
-        "PSScriptTools"
-        "Terminal-Icons"
-        "ZLocation"
-        "Posh-SSH"
-        "posh-git"
-        "Chocolatey"
-        "ExchangeOnlineManagement"
-    )
+Header "Set global settings ..."
 
-    foreach($ModuleToInstall in $ModulesToInstall) {
-        Write-Host ""
-        ExecuteTimed "Install Module $ModuleToInstall" DarkMagenta {
-            Install-Module -Force -Scope CurrentUser -AllowPrerelease -Verbose -Name $ModuleToInstall
-        }
-        Write-Host ""
-    }
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+Set-ExecutionPolicy RemoteSigned -Scope LocalMachine
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
+Header "Install PowerShell modules from gallery ..."
+
+$ModulesToInstall = @(
+    "PSColor"
+    "PSReadLine"
+    "PSScriptTools"
+    "Terminal-Icons"
+    "ZLocation"
+    "Posh-SSH"
+    "posh-git"
+    "Chocolatey"
+    "ExchangeOnlineManagement"
+)
+
+foreach($ModuleToInstall in $ModulesToInstall) {
+    Begin "Install Module $ModuleToInstall"
+    Install-Module -Force -Scope CurrentUser -AllowPrerelease -Verbose -Name $ModuleToInstall
+    End
 }
 
-# cannot install PoShFuck and choco on linux/wsl
 if ($IsLinux) {
+    Write-Host "Skip PoShFuck, winget, chocolatey on linux." -ForegroundColor DarkRed
     exit
 }
 
-ExecuteTimed "Install PowerShell modules from direct source" Cyan {
-    $ModulesToInstall = @(
-        "https://raw.githubusercontent.com/mattparkes/PoShFuck/master/Install-TheFucker.ps1"
-        "https://community.chocolatey.org/install.ps1"
-    )
+Header "Install PowerShell modules from direct source ..."
 
-    foreach($ModuleToInstall in $ModulesToInstall) {
-        Write-Host ""
-        ExecuteTimed "Install module $ModuleToInstall" DarkMagenta {
-            Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($ModuleToInstall)) | Out-Host
-        }
-        Write-Host ""
-    }
+$ModulesToInstall = @(
+    "https://raw.githubusercontent.com/mattparkes/PoShFuck/master/Install-TheFucker.ps1"
+    "https://community.chocolatey.org/install.ps1"
+)
+
+foreach($ModuleToInstall in $ModulesToInstall) {
+    Begin "Install module $ModuleToInstall"
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($ModuleToInstall)) | Out-Host
+    End
 }
 
-ExecuteTimed "Install winget modules" Cyan {
-    $ModulesToInstall = @(
-        "GitHub.cli"
-        "GitHub.GitHubDesktop"
-        "oh-my-posh"
-        "gerardog.gsudo"
-        "GnuWin32.Wget"
-        "Python.Python.2"
-        "Python.Python.3"
-    )
+Header "Install winget modules ..."
 
-    foreach($ModuleToInstall in $ModulesToInstall) {
-        Write-Host ""
-        ExecuteTimed "Install winget module $ModuleToInstall" DarkMagenta {
-            winget install $ModuleToInstall --accept-package-agreements | Out-Host
-        }
-        Write-Host ""
-    }
+$ModulesToInstall = @(
+    "GitHub.cli"
+    "GitHub.GitHubDesktop"
+    "oh-my-posh"
+    "gerardog.gsudo"
+    "GnuWin32.Wget"
+    "Python.Python.2"
+    "Python.Python.3"
+)
+
+foreach($ModuleToInstall in $ModulesToInstall) {
+    Begin "Install winget module $ModuleToInstall"
+    winget install $ModuleToInstall --accept-package-agreements | Out-Host
+    End
 }
 
-ExecuteTimed "Install chocolatey modules" Cyan {
-    $ModulesToInstall = @(
-        "choco-upgrade-all-at-startup"
-        "bat"
-        "less"
-        "dart-sdk"
-    )
+Header "Install chocolatey modules ..."
 
-    foreach($ModuleToInstall in $ModulesToInstall) {
-        Write-Host ""
-        ExecuteTimed "Install chocolatey module $ModuleToInstall" DarkMagenta {
-            choco install $ModuleToInstall -y -v | Out-Host
-        }
-        Write-Host ""
-    }
+$ModulesToInstall = @(
+    "choco-upgrade-all-at-startup"
+    "bat"
+    "less"
+    "dart-sdk"
+)
+
+foreach($ModuleToInstall in $ModulesToInstall) {
+    Begin "Install chocolatey module $ModuleToInstall"
+    choco install $ModuleToInstall -y -v | Out-Host
+    End
 }
 
-ExecuteTimed "Update chocolatey" DarkMagenta {
-    choco upgrade chocolatey -y -v | Out-Host
-}
+Begin "Update chocolatey"
+choco upgrade chocolatey -y -v | Out-Host
+End
 
-ExecuteTimed "Update all chocolatey modules" DarkMagenta {
-    choco upgrade all -y -v | Out-Host
-}
+Begin "Update all chocolatey modules"
+choco upgrade all -y -v | Out-Host
+End
+
+Header "Finished."
+Write-Host ""

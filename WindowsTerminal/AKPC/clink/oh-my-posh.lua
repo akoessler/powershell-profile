5b340528baa98793deb0b4b915033e93122613e8
Andreas Kössler <github@akoessler.com>
2025-08-03 16:35:50 +0200

    Add clink config instructions

---
+++ WindowsTerminal/AKPC/clink/oh-my-posh.lua
@@ -0,0 +1,5 @@
+-- find script directory: clink info
+-- e.g.: C:\Users\akoes\AppData\Local\clink
+-- put this file there
+
+load(io.popen('oh-my-posh init cmd --config="D:/OneDrive/Documents/PowerShell/oh-my-posh-theme.json"'):read("*a"))()
